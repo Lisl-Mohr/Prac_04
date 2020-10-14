2ac926289b30d86d2fb4b01e2fb68ec027df0a98
Lisl-Mohr <lisl.mohr@my.jcu.edu.au>
2020-10-15 08:15:30 +1000

    Commit file

---
+++ prac_08/unreliable_car_test.py
@@ -0,0 +1,19 @@
+"""Unreliable Car Test"""
+
+from prac_08.unreliable_car import UnreliableCar
+
+
+def main():
+    reliable_car = UnreliableCar("Mostly Good", 100, 90)
+    unreliable_car = UnreliableCar("Dodgy", 100, 9)
+
+    for i in range(1, 12):
+        print("Attempting to drive {}km".format(i))
+        print("{:12} drove {:2}km".format(reliable_car.name, reliable_car.drive(i)))
+        print("{:12} drove {:2}km".format(unreliable_car.name, unreliable_car.drive(i)))
+
+    print(reliable_car)
+    print(unreliable_car)
+
+
+main()
